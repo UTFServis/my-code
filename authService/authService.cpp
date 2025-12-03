62c3648dbcd0e5fee0d7e18df2c7f883760b8ed5
rafiei <rafieiorg4@gmail.com>
2025-12-03 15:57:16 +0330

    final

--- authService/authService.cpp
+++ authService/authService.cpp
@@ -1,23 +1,45 @@
 #include "mainHeader.h"
-
-sql::Connection* con = nullptr;
-const string jwtSecretAccess = "MY_ACCESS_SECRET";
-const string jwtSecretRefresh = "MY_REFRESH_SECRET";
-
 #include "signUp.h"
 #include "login.h"
 #include "refreshToken.h"
 #include "deleteUsersAccess.h"
 #include "changeUsersRoleAccess.h"
 #include "allUsersAccess.h"
+#include <cstdlib>
+sql::Connection* con = nullptr;
+const string jwtSecretAccess = "MY_ACCESS_SECRET";
+const string jwtSecretRefresh = "MY_REFRESH_SECRET";
+
 
 
 
 int main() {
     try {
+
+
+        const char* env = getenv("ENV");
+        string dbHost, dbName, dbUser, dbPass;
+
+        if (env && string(env) == "production") {
+            dbHost = "tcp://127.0.0.1:3306";
+            dbName = "testdb";
+            dbUser = "testuser";
+            dbPass = "123456";
+            cout << "Running in Production environment" << endl;
+        } else if(env && string(env) == "sandbox") {
+            dbHost = "127.0.0.1";
+            dbName = "todoList";
+            dbUser = "root";
+            dbPass = "123456";
+            cout << "Running in Sandbox environment" << endl;
+        }else{
+            cout << "enter the API Key " << endl;
+            return 1;
+        }
+
         Driver* driver = get_driver_instance();
-        con = driver->connect("tcp://127.0.0.1:3306", "testuser", "123456");
-        con->setSchema("testdb");
+        Connection* con = driver->connect(dbHost, dbUser, dbPass);
+        con->setSchema(dbName);
 
         Server server;
         server.Post("/signup", signUp);
